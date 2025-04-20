ac009772103f3cbcb0a3b2855e62f29b1ee194dd
connor pearson <34844662+Liconnorli@users.noreply.github.com>
2025-04-20 10:10:14 +0100

    Fixed issues and added a naneinf money joker

--- main.lua
+++ main.lua
@@ -17,21 +17,52 @@ SMODS.Joker{
 	cost = 5,
 	rarity = 1,
 	blueprint_compat = true,
-	eternal_compat = true,
+	eternal_compat = false,
 	unlocked = true,
 	discovered = true,
-	atlas = 'BMJoker',
+	atlas = 'BMjoker',
 	pos = {
         x = 0,
         y = 0
     },
-	config = { extra = { mult = 20 } },
+		loc_vars = function(self, info_queue, card)
+		return { vars = { card.ability.extra.mult } }
+	end,
+	config = { extra = { mult = (10^308)} },
 	calculate = function(self,card,context)
-		if context.joker.main then
+		if context.joker_main then
 			return {
 				mult_mod = card.ability.extra.mult,
 				message = localize { type = 'variable', key = 'a_mult', vars = { card.ability.extra.mult } }
 			}
 		end
 	end
+}
+
+SMODS.Joker {
+	key = 'batthew2',
+	loc_txt = {
+		name = 'Batthew2',
+		text = {
+			"Earn {C:money}$#1#{} at",
+			"end of round"
+		}
+	},
+	config = { extra = { money = (10^308)} },
+	rarity = 1,
+	unlocked = true,
+	discovered = true,
+	atlas = 'BMjoker',
+	pos = { x = 0, y = 0 },
+	cost = 6,
+	loc_vars = function(self, info_queue, card)
+		return { vars = { card.ability.extra.money } }
+	end,
+	-- SMODS specific function, gives the returned value in dollars at the end of round, double checks that it's greater than 0 before returning.
+	calc_dollar_bonus = function(self, card)
+		local bonus = card.ability.extra.money
+		if bonus > 0 then return bonus end
+	end
+	
+	-- Since there's nothing else to calculate, a calculate function is completely unnecessary.
 }
